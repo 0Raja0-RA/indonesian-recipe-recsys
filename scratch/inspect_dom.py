0bc2080b4de146325880122441833de54d4a244c
0raja0-RA <raja.rahmanaziz02@gmail.com>
2026-06-14 14:41:29 +0700

    chore: hapus folder scratch/ dan update .gitignore

--- scratch/inspect_dom.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import re
-
-try:
-    html = urllib.request.urlopen("http://localhost:8501").read().decode("utf-8")
-    testids = set(re.findall(r'data-testid="([^"]+)"', html))
-    print("Found testids:", sorted(list(testids)))
-except Exception as e:
-    print("Error:", e)
